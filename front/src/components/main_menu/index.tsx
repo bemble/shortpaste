a026b7611b6a4565052e8a83228a134283e7f478
Pierre <397503+bemble@users.noreply.github.com>
2022-11-04 00:11:45 +0100

    change how project name is displayed (#6)
    As seen with @adyanth, project fork will be broken from original project, to allow users to fork mine etc.
    Project name remains the same, but its display change to reduce confusion between the to project.

--- front/src/components/main_menu/index.tsx
+++ front/src/components/main_menu/index.tsx
@@ -3,34 +3,76 @@ import classes from "./styles.module.scss";
 import Item from "./item";
 
 import LogoIcon from "../../assets/logo.png";
-import {Link as LinkIcon, TextSnippetOutlined as TextIcon, FilePresent as FileIcon} from '@mui/icons-material';
+import {
+  Link as LinkIcon,
+  TextSnippetOutlined as TextIcon,
+  FilePresent as FileIcon,
+} from "@mui/icons-material";
 import { AppBar, Drawer, Toolbar } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 import AppContext from "../../app_context";
 import classNames from "classnames";
 
-const MainMenu:React.FC = () => {
-  const {t} = useTranslation();
+const MainMenu: React.FC = () => {
+  const { t } = useTranslation();
   const app = useContext(AppContext);
 
-  return <>
-    <AppBar position="fixed" className={classes.root_bottom} sx={{ display: { xs: 'block', md: 'none' }, top: 'auto', bottom: 0 }}>
-      <Toolbar>
-        <Item to="/" color="blue"><LinkIcon />{t("menu.links")}</Item>
-        <Item to="/texts" color="purple"><TextIcon />{t("menu.texts")}</Item>
-        <Item to="/files" color="pink"><FileIcon />{t("menu.files")}</Item>
-      </Toolbar>
-    </AppBar>
-    <Drawer variant="permanent" open={true} className={classes.root_left} sx={{ display: { xs: 'none', md: 'block' } }}>
-        <div className={classes.logo}><img src={LogoIcon} alt="Short{Paste}" /></div>
-        <Item to="/" color="blue"><LinkIcon />{t("menu.links")}</Item>
-        <Item to="/texts" color="purple"><TextIcon />{t("menu.texts")}</Item>
-        <Item to="/files" color="pink"><FileIcon />{t("menu.files")}</Item>
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        className={classes.root_bottom}
+        sx={{ display: { xs: "block", md: "none" }, top: "auto", bottom: 0 }}
+      >
+        <Toolbar>
+          <Item to="/" color="blue">
+            <LinkIcon />
+            {t("menu.links")}
+          </Item>
+          <Item to="/texts" color="purple">
+            <TextIcon />
+            {t("menu.texts")}
+          </Item>
+          <Item to="/files" color="pink">
+            <FileIcon />
+            {t("menu.files")}
+          </Item>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        open={true}
+        className={classes.root_left}
+        sx={{ display: { xs: "none", md: "block" } }}
+      >
+        <div className={classes.logo}>
+          <img src={LogoIcon} alt="ShortPaste" />
+        </div>
+        <Item to="/" color="blue">
+          <LinkIcon />
+          {t("menu.links")}
+        </Item>
+        <Item to="/texts" color="purple">
+          <TextIcon />
+          {t("menu.texts")}
+        </Item>
+        <Item to="/files" color="pink">
+          <FileIcon />
+          {t("menu.files")}
+        </Item>
         <div className={classes.spacer} />
-        <div className={classes.info}><span className={classNames(classes.status, {[classes.up]: app?.status === "up"})} /> {app?.version}</div>
-    </Drawer>
-  </>;
+        <div className={classes.info}>
+          <span
+            className={classNames(classes.status, {
+              [classes.up]: app?.status === "up",
+            })}
+          />{" "}
+          {app?.version}
+        </div>
+      </Drawer>
+    </>
+  );
 };
 
 export default MainMenu;
